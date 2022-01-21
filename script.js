4379db8e047564db7b43909069484be4f6fbc518
Yannick2811 <yanschen90@gmail.com>
2022-01-21 18:12:07 +0100

    Add index.html and style.css and script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const socialsBtn = document.querySelector("#socials-button");
 const footer = document.querySelector("footer");
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbar = document.getElementsByClassName("navbar")[0];
 
 socialsBtn.addEventListener("click", () => {
   footer.scrollIntoView({ behavior: "smooth" });
+});
+
+toggleButton.addEventListener("click", () => {
+  navbar.classList.toggle("active");
 });
